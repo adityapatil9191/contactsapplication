fb1f8a9acac69edba2fb75f68ef1485c7c181cb7
Aditya Patil <adityapatil9191@gmail.com>
2019-09-10 11:46:41 -0700

    sample commit

--- src/app/create-contact/create-contact.component.ts
+++ src/app/create-contact/create-contact.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-contact.component.css']
 })
 export class CreateContactComponent implements OnInit {
-
+  imageUploaded = false;
+  public imagePath;
+  imgURL: any;
+  public message: string;
+  uploadFile=()=>{
+    let myFile = document.getElementById("myFile");
+    myFile.click();
+  }
+  selectFile=(event)=>{
+    if(event.target.files[0].length===0){
+      return
+    }
+    let mimeType = event.target.files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+    var reader = new FileReader();
+    this.imagePath = event.target.files;
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = (_event)=>{
+      this.imgURL = reader.result;
+    }
+    this.imageUploaded = true;
+  }
   constructor() { }
 
   ngOnInit() {
